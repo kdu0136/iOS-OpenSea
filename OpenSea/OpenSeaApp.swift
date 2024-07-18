e9559689e5d56bae97d8df80af713a833e590243
sanhee16 <sinhioa20@gmail.com>
2024-07-18 12:40:23 +0900

    Initial Commit

---
+++ OpenSea/OpenSeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSeaApp.swift
+//  OpenSea
+//
+//  Created by Sandy on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenSeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
